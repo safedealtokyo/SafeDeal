6bff1a0656c3bd0fc08e51dbd78d80038a2f5f36
taaaaho <taaaaho@gmail.com>
2023-04-14 20:35:47 +0900

    feat: deploy safe hooks

--- src/hooks/useSafe.ts
+++ src/hooks/useSafe.ts
@@ -1,6 +1,8 @@
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
 /* eslint-disable consistent-return */
 import SafeApiKit from "@safe-global/api-kit";
-import Safe, { EthersAdapter } from "@safe-global/protocol-kit";
+import Safe, { EthersAdapter, SafeAccountConfig, SafeFactory } from "@safe-global/protocol-kit";
 import { useAddress, useSigner } from "@thirdweb-dev/react";
 import ethers from "ethers";
 
@@ -8,6 +10,7 @@ const useSafe = () => {
   const address = useAddress();
   const signer = useSigner();
 
+  // Base hooks
   const fetchSafeService = () => {
     if (signer) {
       const ethAdapterOwner1 = new EthersAdapter({
@@ -43,8 +46,41 @@ const useSafe = () => {
       return safeSdk;
     }
   };
+
+  const deploySafe = async (workerAddress: string) => {
+    if (signer) {
+      const ethAdapterOwner1 = new EthersAdapter({
+        ethers,
+        signerOrProvider: signer,
+      });
+
+      //   Initialize the Protcol Kit
+      const safeFactory = await SafeFactory.create({
+        ethAdapter: ethAdapterOwner1,
+      });
+
+      // Set Owners
+      const safeAccountConfig: SafeAccountConfig = {
+        owners: [
+          address as string,
+          workerAddress,
+          process.env.NEXT_PUBLIC_OWNER_ADDRESS!, // Owner Address
+        ],
+        threshold: 2,
+      };
+      const safeSdkOwner1 = await safeFactory.deploySafe({ safeAccountConfig });
+
+      const safeAddress = safeSdkOwner1.getAddress();
+      console.log("Your Safe has been deployed:");
+      console.log(`https://goerli.etherscan.io/address/${safeAddress}`);
+      console.log(`https://app.safe.global/gor:${safeAddress}`);
+      // sendEthToSafe(safeAddress)
+    } else {
+      alert("ウォレットが接続されていません");
+    }
+  };
   return {
-    fetchSafeSDK,
+    deploySafe,
   };
 };
 
