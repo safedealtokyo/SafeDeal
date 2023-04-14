6f179ad35de8881a35954206857ef4cbce1156ba
lzxzjtijsp <shirok420@gmail.com>
2023-04-15 07:34:19 +0900

    sbt call

--- src/pages/api/deal/create.ts
+++ src/pages/api/deal/create.ts
@@ -1,19 +1,20 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import { multicallData } from "@/datas/multicallData";
 import { formatFormData } from "@/utils/formatJson";
 import { prisma } from "@/utils/prisma";
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
   const { body } = req;
   if (req.method === "POST") {
     try {
       const sdk = ThirdwebSDK.fromPrivateKey(
-        process.env.PRIVATE_KEY!,
-        process.env.NEXT_PUBLIC_CHAIN!
+          process.env.PRIVATE_KEY!,
+          process.env.NEXT_PUBLIC_CHAIN!
       );
       const options = {
         name: body.title,
@@ -37,10 +38,11 @@ export default async function handler(
 
       const contract = await sdk.getContract(contractAddress);
       const txResult = await contract.roles.grant(
-        "minter",
-        body.walletAddress
+          "minter",
+          body.walletAddress
       );
 
+      const tx = await contract.call("multicall", [multicallData]);
       return res.status(200).json({
         contractAddress,
         txResult
