14c74f9316344c0fadc8225fbcb759829ee4cfbe
taaaaho <taaaaho@gmail.com>
2023-04-15 05:03:08 +0900

    feat: chat component

---
+++ src/components/Chat/Header.tsx
@@ -0,0 +1,25 @@
+import { Flex, Avatar, AvatarBadge, Text } from "@chakra-ui/react";
+import React from "react";
+
+import { addressFormat } from "@/utils/format";
+
+type Props = {
+  name: string;
+};
+function Header({ name }: Props) {
+  return (
+    <Flex w="100%">
+      <Avatar size="lg" name="Dan Abrahmov" src="https://bit.ly/dan-abramov">
+        <AvatarBadge boxSize="1.25em" bg="green.500" />
+      </Avatar>
+      <Flex flexDirection="column" mx="5" justify="center">
+        <Text fontSize="lg" fontWeight="bold">
+          {addressFormat(name)}
+        </Text>
+        {/* <Text color="green.500">Online</Text> */}
+      </Flex>
+    </Flex>
+  );
+}
+
+export default Header;
---
+++ src/components/Chat/Messages.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/no-unstable-nested-components */
+import { Avatar, Flex, Text } from "@chakra-ui/react";
+import { useAddress } from "@thirdweb-dev/react";
+import React, { useEffect, useRef } from "react";
+
+export type Message = {
+  text: string;
+  from: string;
+};
+type Props = {
+  messages: Message[];
+};
+function Messages({ messages }: Props) {
+  const address = useAddress();
+  //   function AlwaysScrollToBottom() {
+  //     const elementRef = useRef();
+  //     useEffect(() => elementRef.current.scrollIntoView());
+  //     return <div ref={elementRef} />;
+  //   }
+
+  return (
+    <Flex w="100%" h="80%" overflowY="scroll" flexDirection="column" p="3">
+      {messages.map((item, index) => {
+        if (item.from === address) {
+          return (
+            <Flex key={index} w="100%" justify="flex-end">
+              <Flex
+                bg="black"
+                color="white"
+                minW="100px"
+                maxW="350px"
+                my="1"
+                p="3"
+              >
+                <Text>{item.text}</Text>
+              </Flex>
+            </Flex>
+          );
+        }
+        return (
+          <Flex key={index} w="100%">
+            <Avatar
+              name="Computer"
+              src="https://avataaars.io/?avatarStyle=Transparent&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
+              bg="blue.300"
+            />
+            <Flex
+              bg="gray.100"
+              color="black"
+              minW="100px"
+              maxW="350px"
+              my="1"
+              p="3"
+            >
+              <Text>{item.text}</Text>
+            </Flex>
+          </Flex>
+        );
+      })}
+      {/* <AlwaysScrollToBottom /> */}
+    </Flex>
+  );
+}
+
+export default Messages;
