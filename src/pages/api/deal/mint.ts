8bef965be49818cd36950168972923644e6a81c9
lzxzjtijsp <shirok420@gmail.com>
2023-04-15 02:13:37 +0900

    Mint用Metadata API

--- src/pages/api/deal/mint.ts
+++ src/pages/api/deal/mint.ts
@@ -1,27 +1,29 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { formatFormData } from "@/utils/formatJson";
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { body } = req;
+  const { contractAddress, destinationAddress } = req.body;
   if (req.method === "POST") {
     try {
       const sdk = ThirdwebSDK.fromPrivateKey(
         process.env.PRIVATE_KEY!,
         process.env.NEXT_PUBLIC_CHAIN!
       );
-      const options = {
-        name: body.title,
-        description: formatFormData(body).toString(),
-        primary_sale_recipient: process.env.NEXT_PUBLIC_PRIMARIY_SALES_ADDRESS!,
-      };
-      const contractAddress = await sdk.deployer.deployNFTCollection(options);
 
-      return res.status(200).json(contractAddress);
+      // あとで変える
+      const metadata = {
+        name: "実績証明",
+        description: "実績証明",
+      };
+      const contract = await sdk.getContract<"nft-collection">(contractAddress);
+      const txResult = await contract.erc721.mintTo(
+        destinationAddress,
+        metadata
+      );
+      return res.status(200).json(txResult);
     } catch (error) {
       console.log(error);
       return res.status(500).json(error);
