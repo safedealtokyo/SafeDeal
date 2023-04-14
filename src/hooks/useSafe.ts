dd32ccabf6e0e5bcd5e0d5ab40eb5cf1fe2f0963
taaaaho <taaaaho@gmail.com>
2023-04-14 20:38:44 +0900

    feat: propose transaction with safe

--- src/hooks/useSafe.ts
+++ src/hooks/useSafe.ts
@@ -3,6 +3,7 @@
 /* eslint-disable consistent-return */
 import SafeApiKit from "@safe-global/api-kit";
 import Safe, { EthersAdapter, SafeAccountConfig, SafeFactory } from "@safe-global/protocol-kit";
+import { SafeTransactionDataPartial } from "@safe-global/safe-core-sdk-types";
 import { useAddress, useSigner } from "@thirdweb-dev/react";
 import ethers from "ethers";
 
@@ -47,6 +48,7 @@ const useSafe = () => {
     }
   };
 
+  // Create safe multisig contract for deposit.
   const deploySafe = async (workerAddress: string) => {
     if (signer) {
       const ethAdapterOwner1 = new EthersAdapter({
@@ -76,11 +78,46 @@ const useSafe = () => {
       console.log(`https://app.safe.global/gor:${safeAddress}`);
       // sendEthToSafe(safeAddress)
     } else {
-      alert("ウォレットが接続されていません");
+      alert("Wallet not connected");
+    }
+  };
+
+  const proposeTransaction = async (destinationAddress: string, withdrawAmount: string) => {
+    // Any address can be used. In this example you will use vitalik.eth
+    const destination = destinationAddress;
+    const amount = ethers.utils.parseUnits(withdrawAmount, "ether").toString();
+    const safeService = fetchSafeService();
+    const safeSdk = await fetchSafeSDK();
+    const safeTransactionData: SafeTransactionDataPartial = {
+      to: destination,
+      data: "0x",
+      value: amount,
+    };
+
+    if (signer && address && safeService && safeSdk) {
+      // Create a Safe transaction with the provided parameters
+      const safeTransaction = await safeSdk.createTransaction({
+        safeTransactionData,
+      });
+
+      // Deterministic hash based on transaction parameters
+      const safeTxHash = await safeSdk.getTransactionHash(safeTransaction);
+
+      // Sign transaction to verify that the transaction is coming from owner 1
+      const senderSignature = await safeSdk.signTransactionHash(safeTxHash);
+      const safeAddress = await fetchSafe();
+      await safeService.proposeTransaction({
+        safeAddress: safeAddress!,
+        safeTransactionData: safeTransaction.data,
+        safeTxHash,
+        senderAddress: address,
+        senderSignature: senderSignature.data,
+      });
     }
   };
   return {
     deploySafe,
+    proposeTransaction,
   };
 };
 
