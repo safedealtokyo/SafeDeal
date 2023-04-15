906192cb0b29286534c6d6559a60c34e3b30a6ad
taaaaho <taaaaho@gmail.com>
2023-04-15 15:21:55 +0900

    feat: client confirmation

--- src/pages/deal/index.tsx
+++ src/pages/deal/index.tsx
@@ -6,7 +6,7 @@ import {
   Heading,
   SimpleGrid,
   Text,
-  VStack
+  VStack,
 } from "@chakra-ui/react";
 import { Deal } from "@prisma/client";
 import { NextPageContext } from "next";
@@ -30,23 +30,25 @@ export default function Protected({ deals }: Props) {
       <Heading>Dealページ</Heading>
       <Center>
         <SimpleGrid columns={4} spacing="24px">
-          {tempDeals.map((deal) => (
-            <Link key={deal.id} href={`/deal/${deal.id}`}>
-              <Card width="200px" py="20px">
-                <VStack>
-                  <Text>{deal.title}</Text>
-                  <Text>{deal.fixedFee} ETH</Text>
-                </VStack>
-              </Card>
-            </Link>
-          ))}
+          {tempDeals
+            .filter((deal) => deal.multiSigAddress === null)
+            .map((deal) => (
+              <Link key={deal.id} href={`/deal/${deal.id}`}>
+                <Card width="200px" py="20px">
+                  <VStack>
+                    <Text>{deal.title}</Text>
+                    <Text>{deal.fixedFee} ETH</Text>
+                  </VStack>
+                </Card>
+              </Link>
+            ))}
         </SimpleGrid>
       </Center>
       <MintButton
         contractAddress="0xB9b6A92f52b2fcb7CE3f71b90Cf1793101753eC0"
         buttonLabel="取引終了"
       />
-      <MintButtonFromWallet contractAddress="0x8A8036b5fe57f7347A8B80C3d364557E28b8fd6F" />
+      {/* <MintButtonFromWallet contractAddress="0x8A8036b5fe57f7347A8B80C3d364557E28b8fd6F" /> */}
     </Box>
   );
 }
@@ -55,7 +57,7 @@ export async function getServerSideProps(context: NextPageContext) {
   const deals = await fetchList();
   return {
     props: {
-      deals: JSON.stringify(deals)
-    }
+      deals: JSON.stringify(deals),
+    },
   };
 }
